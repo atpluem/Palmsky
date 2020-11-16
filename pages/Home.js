ce044218677d31d3634eebe1e5a6371311ad3942
atpluem <atpluem@gmail.com>
2020-11-16 21:27:34 +0700

    Home nav header

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import Navbar from '../home/Nav'
-import Header from '../home/Header'
+import Navbar from './Nav'
+import Header from './Header'
 
 export default function Home() {
   return (
