54c5d8de9e9ab5f73741c44091649f66b3284e83
krittanan chalong <krittananchalong@gmail.com>
2020-11-16 21:30:47 +0700

    last version of ice

---
+++ pages/owner/insertData.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function insertData () {
+    const number = [1, 2, 3, 4]
+
+    return (
+        <div>
+            <img src="https://i.pinimg.com/236x/34/3a/f9/343af9a64d1875d2ff718a65767f74aa.jpg" />
+            <p>Thagoon Thongpee</p>
+            <p>PALMSKY BOOK HOUSE</p>
+            <p></p>
+        </div>
+    )
+}
+
