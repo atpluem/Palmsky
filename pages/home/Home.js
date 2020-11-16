cb986537b6b381e72904c20f33da291a0c9a1d27
atpluem <atpluem@gmail.com>
2020-11-16 21:14:26 +0700

    Home

---
+++ pages/home/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Navbar from '../home/Nav'
+import Header from '../home/Header'
+
+export default function Home() {
+  return (
+    <div>
+
+      <Header />
+      <Navbar />
+
+      <main>
+        <h1>Hi Bro!</h1>
+
+      </main>
+    </div>
+  )
+}
