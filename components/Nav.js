9bc2f0246118fb0bbbfd2b13b197e242e8a78a21
krittanan chalong <krittananchalong@gmail.com>
2020-11-21 03:27:36 +0700

    Add dropdown but unfinish

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div style = {{ position: '-webkit-sticky', position: "sticky", top: 0, zIndex: 2 }}>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <Link href="/Home">
           <a className="navbar-brand">PALMSKY</a>
         </Link>
--- pages/category/library.js
+++ pages/category/library.js
@@ -5,7 +5,7 @@ import Tags from './tags'
 
 const library = () => {
 
-    const category  = ["Cartoon", "Fiction", "Fantacy", "Romance", "Adventure" ]
+    const category = ["Cartoon", "Fiction", "Fantacy", "Romance", "Adventure"]
 
     return (
         <div>
@@ -17,15 +17,25 @@ const library = () => {
                 w
             </div>
 
+            {/* Category Drop */}
+            <div className="container d-flex flex-row">
+                <p className="h4 mt-1 mr-2 font-weight-bold">LIBRARY </p>
+                <div className="dropdown">
+                    <button className = "btn btn-secondary dropdown-toggle" id = "dropMenu" type="button" aria-haspopup = "true" aria-expanded = "false">Category</button>
+                    <div className = "dropdown-menu" aria-labelledby="dropMenu">
+                        <a className = "dropdown-item" href ="#" >Selector 1</a>
+                    </div>
+                </div>
+            </div>
 
             {/* Book Category */}
-            <div className = "container">
+            <div className="container">
                 {category.map(name => (<Tags />))}
             </div>
 
             {/* footer */}
-            <div className = "bg-dark mt-5 position-relative" >
-                <p className = "text-light text-center font-weight-lighter " >@2020-20221 PALMSKY THAILAND</p>
+            <div className="bg-dark mt-5 position-relative" >
+                <p className="text-light text-center font-weight-lighter " >@2020-20221 PALMSKY THAILAND</p>
             </div>
         </div>
     )
