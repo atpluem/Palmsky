4aee0d9e54dc851b8279dac91f3b73db549ec81e
atpluem <atpluem@gmail.com>
2020-11-20 16:54:40 +0700

    Home update

--- components/HomeCat.js
+++ components/HomeCat.js
@@ -13,43 +13,53 @@ export default function HomeCat() {
 
   return (
     <div>
-      <Carousel breakPoints={breakPoints} className={styles.container}>
-        <div className={styles.item}>
-          <img
-            src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
-            height="300"
-            alt=""
-          />
-        </div>
-        <div className={styles.item}>
-          <img
-            src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
-            height="300"
-            alt=""
-          />
-        </div>
-        <div className={styles.item}>
-          <img
-            src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
-            height="300"
-            alt=""
-          />
-        </div>
-        <div className={styles.item}>
-          <img
-            src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
-            height="300"
-            alt=""
-          />
-        </div>
-        <div className={styles.item}>
-          <img
-            src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
-            height="300"
-            alt=""
-          />
-        </div>
-      </Carousel>
+      <div className={styles.catName}>Category</div>
+      <div className={styles.container}>
+        <Carousel breakPoints={breakPoints} itemPadding={[40, 0]}>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+          <div className={styles.item}>
+            <img
+              src="https://www.playinone.com/wp-content/uploads/2019/12/%E0%B8%AA%E0%B8%B0%E0%B8%94%E0%B8%B8%E0%B8%94%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A2%E0%B8%B1%E0%B8%A2%E0%B9%81%E0%B8%9F%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B9%88%E0%B8%B2-11.jpg"
+              height="300"
+              alt=""
+            />
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
 }
